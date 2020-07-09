a140695714122cc0686928872d70c9dedb2656e7
sergey <remote.c.dzencode@gmail.com>
2020-07-09 08:57:26 +0300

    setup

--- app/Number.php
+++ app/Number.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Number extends Model
 {
+    use SoftDeletes;
 
     protected $fillable = [
         'number',
